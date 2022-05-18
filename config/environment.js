92fde5ce5b70cb2f97fc777658c1a4e12fc15256
kemo99 <karim.ali@live.com>
2022-05-18 13:07:41 +0100

    Heroku Deployment

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const port = process.env.PORT || 3000;
-
-export const dbURL = process.env.dbURL || 'mongodb://127.0.0.1:27017/project-3';
-export const secret = process.env.SECRET || 'pineapples';
+export const DB_URI = process.env.DB_URI || 'mongodb://localhost/project-0';
+export const port = process.env.PORT || 4000;
+export const secret = process.env.SECRET || 'shhhh its a secret';
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { port } from './config/environment.js';
 import router from './config/router.js';
 import { connectToDb } from './db/helpers.js';
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use('/api', router);
 
 async function runServer() {
